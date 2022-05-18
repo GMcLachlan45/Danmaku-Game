1374c1a9b8276abf7ddeee68a89bce2ca4400db2
GMcLachlan45 <gmclachlan45@gmail.com>
2022-05-17 20:58:41 -0700

    Add the main game class for use one day

---
+++ src/model/game.h
@@ -0,0 +1,10 @@
+
+class Game{
+
+    public:
+        //Probably stuff like the stage and the array of  entities
+        //score too as well as lives and other integers
+        //And game states too.
+        Game();
+        ~Game();
+};
