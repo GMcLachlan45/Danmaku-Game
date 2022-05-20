b572f9c6cb66d09f5b5ad05f7949d4dfaa7e9ac4
GMcLachlan45 <gmclachlan45@gmail.com>
2022-05-19 21:46:41 -0700

    revamp the timing and other systems

--- src/main.cpp
+++ src/main.cpp
@@ -20,62 +20,61 @@ float rgb[]= {0.0f, 0.0f, 0.0f};
 int main() {
     auto time = std::chrono::system_clock::now();
     //Initialize view
-    Renderer rend;
+    Renderer view;
+    Renderer *viewPtr = &view;
 
-    //Initialize model 
-    Game ng;
+    //Initialize model
+    Game model;
+    Game *modelPtr = &model;
 
     //Initialize control
     InputHandler control;
-    InputHandler *cont = &control;
+    InputHandler *controlPtr = &control;
 
     //Create a window object
-    if (rend.window_fails()) {
+    if (viewPtr->window_fails()) {
         return -1;
     }
 
 
     //Notify GLFW to set the context of our window as the main context of the current thread
-    glfwMakeContextCurrent(rend.window);
+    glfwMakeContextCurrent(viewPtr->window);
     //Register a callback function for the window. Whenever the window changes size, GLFW will call this function and fill in the corresponding parameters for your processing.
-    glfwSetFramebufferSizeCallback(rend.window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(viewPtr->window, framebuffer_size_callback);
     //Initialize the function pointer used by GLAD to manage OpenGL
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
- 
+
     //Initializes the player
-    
+
 
 	const unsigned char* glVer = glGetString(GL_VERSION);
 		std::cout << glVer << std::endl;
-	
+
     bool game_exit = false;
     //Rendering loop
-    std::chrono::duration<double> delta = std::chrono::system_clock::now()-time;
-    while(!glfwWindowShouldClose(rend.window)&&!game_exit) {
-        time = std::chrono::system_clock::now();
+    while(!glfwWindowShouldClose(viewPtr->window)&&!game_exit) {
         // Control
-        control.process_input(rend.window, rgb);
+        controlPtr->process_input(viewPtr->window);
 
+
+        // get how long loop takes
         // Update the Game (model)
-        ng.updateGame(cont, 2);
-
+        modelPtr->update_game(controlPtr, (float) (std::chrono::system_clock::now()-time).count());
+        time = std::chrono::system_clock::now();
         // Display
 
         // Rendering instructions
         glClearColor(rgb[0], rgb[1], rgb[2], 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         // Check and call event, exchange buffer
-        glfwSwapBuffers(rend.window);//Check the trigger event
+        glfwSwapBuffers(viewPtr->window);//Check the trigger event
         glfwPollEvents();    //Exchange color buffer
 
-        // get how long loop takes
-        delta = std::chrono::system_clock::now()-time;
     }
 
-    std::cout << (1.0/(double) delta.count()) << std::endl;
     //Release/delete all resources allocated before
     return EXIT_SUCCESS;
 }
@@ -84,7 +83,7 @@ int main() {
 // When the user changes the size of the window, the viewport should also be adjusted
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     // Note: For Retina display, the width and height will be significantly higher than the original input value.
-    
+
     std::cout << "changed to " << width << " by " << height << std::endl;
     glViewport(0, 0, width, height);
 }
