9b4d0ce26b781190015b20950df81a5ddd7e1928
GMcLachlan45 <gmclachlan45@gmail.com>
2022-05-21 14:12:17 -0700

    update the final test shaders

--- src/view/shaders/vertexShader.glsl
+++ src/view/shaders/vertexShader.glsl
@@ -5,5 +5,5 @@ out vec4 vertexColor; // sending color info to the frag shader
 
 void main(){
     gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
-    vertexColor = vec4(0.0, 0.5, 0.0, 1.0); // set the output variable to a dark-red color
+    vertexColor = vec4(0.0, 0.5, 0.0, 1.0);
 }
