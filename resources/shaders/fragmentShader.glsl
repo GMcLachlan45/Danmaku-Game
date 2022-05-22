a39a45220c4aa3f090529d03e94b3dc9b1f2eb6d
GMcLachlan45 <gmclachlan45@gmail.com>
2022-05-21 19:30:28 -0700

    move the shaders to resources

--- resources/shaders/fragmentShader.glsl
+++ resources/shaders/fragmentShader.glsl
@@ -1,8 +1,8 @@
 #version 330 core
-out vec4 FragColor;
+out vec4 color;
   
 in vec4 vertexColor; // the input variable from the vertex shader (same name and same type)  
 
 void main(){
-    FragColor = vertexColor;
+    color = vec4(0.0, 1.0, 0.0, 1.0);
 } 
