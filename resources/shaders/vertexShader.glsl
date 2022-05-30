7d1f8040a6a992c2fc446a8fa2037f32412c8674
GMcLachlan45 <gmclachlan45@gmail.com>
2022-05-29 20:45:26 -0700

    LITERALLY HOW? IT'S THE SAME BUT NOT WORKING???

--- resources/shaders/vertexShader.glsl
+++ resources/shaders/vertexShader.glsl
@@ -1,12 +1,12 @@
 #version 330 core
 layout(location = 0) in vec3 position; // the position variable has attribute position 0
-//layout(location = 1) in vec3 inColor; // the color has attribute position 1
+layout(location = 1) in vec3 inColor; // the color has attribute position 1
   
 // here is where we will eventually import the matrix to move the thing around (mat3)
 
-out vec4 vertexColor; // sending color info to the frag shader
+//out vec4 vertexColor; // sending color info to the frag shader
 
 void main(){
     gl_Position = vec4(position, 1.0); // see how we directly give a vec3 to vec4's constructor mat * position
-    vertexColor = vec4(1.0,1.0,1.0, 1.0);
+  //  vertexColor = vec4(0.0,1.0,1.0, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ int main() {
     Renderer view;
     Renderer *viewPtr = &view;
 
+	
     //Initialize model
     Game model;
     Game *modelPtr = &model;
@@ -41,6 +42,7 @@ int main() {
 
     //Notify GLFW to set the context of our window as the main context of the current thread
     glfwMakeContextCurrent(viewPtr->window);
+    
     //Register a callback function for the window. Whenever the window changes size, GLFW will call this function and fill in the corresponding parameters for your processing.
     glfwSetFramebufferSizeCallback(viewPtr->window, framebuffer_size_callback);
     
@@ -50,7 +52,44 @@ int main() {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+
+	                //		x		y	  z		rgb
+    float vertices[6] = {-0.5f,  0.5f ,// 0.0f, //0.0f, 0.0f, 0.0f,
+                          -0.5f, -0.5f,// 0.0f, //0.0f, 0.0f, 0.0f,
+                           0.5f, -0.5f};//,// 0.0f, //0.0f, 0.0f, 0.0f,
+						  // 0.5f,  0.5f};//,// 0.0f};//, 0.0f, 0.0f, 0.0f};
+
+    //Vertex Buffer
+    GLuint vertexBufferObject;
+    glGenBuffers(1, &vertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW); //Sends Vertices to the GPU
 	
+	//Position
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+//						tl bl br tr
+  //  GLuint indices[6] = {0,1,2,3};
+//	
+//	// Index Buffer
+  //  GLuint indexBufferObject;
+  //  glGenBuffers(1, &indexBufferObject);
+//
+//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+  //  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+	
+	
+	
+
+
+	
+
+//	// Color
+//    glEnableVertexAttribArray(1);
+//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3 * sizeof(float)));
+
 	
 	// initialize shaders
 	
@@ -59,77 +98,30 @@ int main() {
         "../resources/shaders/fragmentShader.glsl");
 
     glUseProgram(shader);
-	
-	
-	
-						
-						
-					//		x		y	  z		rgb
-    float vertices[24] = {-0.5f,  0.5f, 0.0f,// 0.0f, 0.0f, 0.0f,
-                          -0.5f, -0.5f, 0.0f,// 0.0f, 0.0f, 0.0f,
-                           0.5f, -0.5f, 0.0f,// 0.0f, 0.0f, 0.0f,
-						   0.5f,  0.5f, 0.0f};//, 0.0f, 0.0f, 0.0f};
-
-    //Vertex Buffer
-    GLuint vertexBufferObject;
-    glGenBuffers(1, &vertexBufferObject);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //Sends Vertices to the GPU
-	
-	
-	
-//						tl bl br tr
-    GLuint indices[6] = {0,1,2,3};
-	
-	// Index Buffer
-    GLuint indexBufferObject;
-    glGenBuffers(1, &indexBufferObject);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
-	
-	
-	
-
-	// Position
-    glEnableVertexAttribArray(0);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), 0);
-	
-
-	// Color
-   // glEnableVertexAttribArray(1);
-    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3 * sizeof(float)));
 
 
-
-
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
     //Initializes the player
-	const unsigned char* glVer = glGetString(GL_VERSION);
-		std::cout << glVer << std::endl;
-
     bool game_exit = false;
     //Rendering loop
     while(!glfwWindowShouldClose(viewPtr->window)&&!game_exit) {
         // Control
-        controlPtr->process_input(viewPtr->window);
+      //  controlPtr->process_input(viewPtr->window);
 
         // get how long loop takes
         // Update the Game (model)
-        modelPtr->update_game(controlPtr, (float) (std::chrono::system_clock::now()-time).count());
-        time = std::chrono::system_clock::now();
+       // modelPtr->update_game(controlPtr, (float) (std::chrono::system_clock::now()-time).count());
+       // time = std::chrono::system_clock::now();
         // Display
 
         // Rendering instructions
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);//rgb[0], rgb[1], rgb[2], 1.0f);
+        glClearColor(0.5f, 0.0f, 0.0f, 1.0f);//rgb[0], rgb[1], rgb[2], 1.0f);
 		
-		
-        glDrawElements(GL_LINE_LOOP, 4, GL_UNSIGNED_INT, (void*)0);
+		glDrawArrays(GL_LINE, 0, 3);
+        //glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, (void*)0);
 
         glfwSwapBuffers(viewPtr->window);
         
