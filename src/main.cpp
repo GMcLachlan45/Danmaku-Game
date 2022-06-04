c05f4d0c10c7be5086ba8a6ae1525e527a220c20
GMcLachlan45 <gmclachlan45@gmail.com>
2022-06-04 12:16:58 -0700

    start right from scratch

--- src/main.cpp
+++ src/main.cpp
@@ -1,117 +1,116 @@
 // System Headers
-#include "view/renderer.h"
-#include "model/game.h"
-#include "controller/process.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 // Standard Headers
 #include <cstdio>
 #include <cstdlib>
-#include <chrono>
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);//Callback function prototype declaration
-
+void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_HEIGHT = 800;
 
-float rgb[]= {0.0f, 0.0f, 0.0f};
+float r = 0.0f; 
+float g = 0.0f; 
+float b = 0.0f; 
 
 int main() {
-    auto time = std::chrono::system_clock::now();
-    //Initialize view
-    Renderer view;
-    Renderer *viewPtr = &view;
+    //Initialize GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Create a window object
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "GLFW 3.3.1", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-    //Initialize model
-    Game model;
-    Game *modelPtr = &model;
-
-    //Initialize control
-    InputHandler control;
-    InputHandler *controlPtr = &control;
-
+    //Notify GLFW to set the context of our window as the main context of the current thread
+    glfwMakeContextCurrent(window);
+    //Register a callback function for the window. Whenever the window changes size, GLFW will call this function and fill in the corresponding parameters for your processing.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     //Initialize the function pointer used by GLAD to manage OpenGL
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-
-    //Initializes the player
-	const unsigned char* glVer = glGetString(GL_VERSION);
+    const unsigned char* glVer = glGetString(GL_VERSION);
 		std::cout << glVer << std::endl;
 
-    //Create a window object
-    if (viewPtr->window_fails()) {
-        std::cout<< "window failed" << std::endl;
-        return -1;
-    }
 
+
+    // start with all of the buffers
+
+	GLuint indexBufferObject;
+    glGenBuffers(1, &indexBufferObject);
+
+
+
+
+
+
+
+
+
+
+    //Initializes the player
     bool game_exit = false;
-    //Rendering loop
-    while(!glfwWindowShouldClose(viewPtr->window)&&!game_exit) {
-        // Control
-        controlPtr->process_input(viewPtr->window);
 
-        // get how long loop takes
-        // Update the Game (model)
-        modelPtr->update_game(controlPtr, (float) (std::chrono::system_clock::now()-time).count());
-        time = std::chrono::system_clock::now();
-        // Display
+
+
+    //Rendering loop
+    while(!glfwWindowShouldClose(window)&&!game_exit) {
+		
+		
+        // input
+        processInput(window);
+
 
         // Rendering instructions
-        glClearColor(rgb[0], rgb[1], rgb[2], 1.0f);
-        
-        viewPtr->render();
-        
-        //Check the trigger event
-        glfwPollEvents();    
-
+        glClearColor(r, g, b, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Check and call event, exchange buffer
+        glfwSwapBuffers(window);//Check the trigger event
+        glfwPollEvents();    //Exchange color buffer
     }
-
     //Release/delete all resources allocated before
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
 
+//Input control, check if the user presses the arrow keys
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        std::cout<< "Pushed up" <<std::endl;
+        r+=0.004;
+    }
+    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        std::cout<< "Pushed down" <<std::endl;
+    }
+    if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+        std::cout<< "Pushed left" <<std::endl;
+        g+=0.004;
+    }
+    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+        std::cout<< "Pushed right" <<std::endl;   
+        b+=0.004;     
+    }
+    if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS){
+        std::cout << "Pushed shift" <<std::endl;
+        r=g=b=0.0f;
+    }
+}
 
 // When the user changes the size of the window, the viewport should also be adjusted
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     // Note: For Retina display, the width and height will be significantly higher than the original input value.
-
-    std::cout << "changed to " << width << " by " << height << std::endl;
     glViewport(0, 0, width, height);
 }
-
-
-
-
-
-
-
-    // std::cout << "Got 1" << std::endl;
-
-    // float positions[6] = {-0.5f, -0.5f,
-    //                      -0.5f, 0.5f,
-    //                       0.0f, 0.5f};
-    
-    // std::cout << "Got 2" << std::endl;
-
-    // GLuint buffer;
-
-    // std::cout << "Got 3" << std::endl;
-    // glGenBuffers(1, &buffer); 
-
-    // std::cout << "Got 4" << std::endl;
-    // glBindBuffer(GL_ARRAY_BUFFER, buffer);
-
-    // std::cout << "Got 5" << std::endl;
-    // glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
-    // std::cout << "Got 6" << std::endl;
-
-    // glEnableVertexAttribArray(0);
-    // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
-
-
-    // GLuint shader = Renderer::createShader("vertexShader", "fragmentShader");
