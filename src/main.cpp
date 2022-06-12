7cc2c167c5c8b7d31d7610bf821c91ffeb437911
GMcLachlan45 <gmclachlan45@gmail.com>
2022-06-12 16:02:57 -0700

    Add the ShaderCompiler to the CMake compilation

--- src/main.cpp
+++ src/main.cpp
@@ -9,78 +9,7 @@
 #include <string>
 #include <fstream>
 
-//helper method to read shaders
-std::string readFromFile(const std::string filename){
-    std::string shaderData;
-
-
-    std::ifstream fileStream(filename, std::ios::in);
-    //tries to open the glsl files
-    if(!fileStream.is_open()) { //thow an error if we can't read the file. 
-        std::cerr << "Could not read file " << filename << ". File does not exist." << std::endl;
-        return "";
-    }
-
-    // read the lines and put it into  a string
-    std::string line = "";
-    while(!fileStream.eof()) {
-        std::getline(fileStream, line);
-        shaderData.append(line + "\n");
-    }
-
-    fileStream.close();
-    return shaderData;
-    };
-
-GLuint compileShader(GLuint shaderType, const std::string sourceFile){
-
-    GLuint id = glCreateShader(shaderType);
-
-    // Get the char array for the source data of the glsl
-    const std::string src = readFromFile(sourceFile);
-    const char * source = src.c_str();
-    std::cout<<( (GL_VERTEX_SHADER==shaderType) ? "vert shader \n": "frag shader\n") <<" "<<source<<std::endl;
-
-    glShaderSource(id, 1, &source, nullptr);
-    glCompileShader(id);
-
-
-    //error Checking for the shader Compilation
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-
-    if(!result){ // if the compilation fails, say where and why
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length* sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << message<< std::endl;
-        glDeleteShader(id);
-        return 0;
-    }
-
-    return id;
-    };
-
-// take in the shader names to be searched for in shader file
-GLuint createShader(const std::string vertexShaderFile, const std::string fragmentShaderFile){
-    //create the shader program we're going to use
-    GLuint program = glCreateProgram();
-    GLuint vertShader = compileShader(GL_VERTEX_SHADER, vertexShaderFile);
-    GLuint fragShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderFile);
-
-    // connect the shader to the program
-    glAttachShader(program, vertShader);
-    glAttachShader(program, fragShader);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vertShader);
-    glDeleteShader(fragShader);
-
-    return program;
-};
-
+#include "view/ShaderCompiler.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -126,7 +55,7 @@ int main()
 
     // build and compile our shader program
     // link shaders
-    GLuint shaderProgram = createShader("../resources/shaders/vertexShader.glsl", "../resources/shaders/fragmentShader.glsl");
+    GLuint shaderProgram = ShaderCompiler::createShader("../resources/shaders/vertexShader.glsl", "../resources/shaders/fragmentShader.glsl");
     
     glLinkProgram(shaderProgram);
 
